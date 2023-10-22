6b10ad4876ab82247b4be799bdf691d30b69cb89
Charles Geier <geiercf@gmail.com>
2023-10-22 17:20:00 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,11 @@ release = '1.0'
 # for a list of supported languages.
 #language = None
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #today = ''
